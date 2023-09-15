51a3fba234aa9746178332f030e73f0757599655
caseybetts <caseyandsusana@gmail.com>
2023-09-15 15:54:08 -0600

    adds plotting functionality

--- pri_scheme.py
+++ pri_scheme.py
@@ -38,6 +38,9 @@ test_cases = [
     [.007, .007, .007, .007, .007, .007, .007, .007, .008, .008, .008, .008, .008],
     [.007, .007, .007, .007, .007, .007, .007, .007, .007, .008, .008, .008, .008],
     [.007, .007, .007, .007, .007, .007, .007, .007, .007, .007, .008, .008, .008],
+    [.007, .007, .007, .007, .007, .007, .007, .007, .007, .007, .007, .008, .008],
+    [.007, .007, .007, .007, .007, .007, .007, .007, .007, .007, .007, .007, .008],
+    [.007, .007, .007, .007, .007, .007, .007, .007, .007, .007, .007, .007, .007],
 ]
 
 class Revenue_Calculator:
@@ -50,11 +53,15 @@ class Revenue_Calculator:
         self.cloud_cover_values_location = "cloudcover.csv"
 
         # Set variables
-        self.initial_priorities = [.0076]*12
+        self.initial_priorities = [ 7.305e-03,  7.527e-03,  7.431e-03,  7.570e-03,  7.688e-03, 
+                                   7.488e-03,  7.683e-03,  7.699e-03,  7.914e-03,  7.650e-03,
+                                    7.576e-03,  7.815e-03]
         self.number_of_scenarios = 1
         self.bounds = [(.007,.008) for x in range(12)]
         self.cloud_cover_buckets = {}
         self.scale = 100000
+        self.clear_columns = 10
+        self.dollar_breaks = [20, 15, 12, 10, 8, 6, 4, 3, 2, 1, 0]
 
         # Score Curve Variables
         self.coefficient = .47
@@ -95,7 +102,6 @@ class Revenue_Calculator:
         self.active_orders["Score"] = 0
         self.active_orders["Total_Score"] = 0
         self.active_orders["Scheduled"] = False 
-        self.active_orders["Clear"] = False
     
     def create_cloud_cover_buckets(self):
         """ Populates a dictionary where the keys are each active latitude and the values are lists of possible cloud cover values """
@@ -109,6 +115,22 @@ class Revenue_Calculator:
 
         self.active_orders.Predicted_CC = self.active_orders.apply( lambda x: choice(self.cloud_cover_buckets[x.Latitude]), axis=1)
 
+    def set_clear_orders(self):
+        """ Randomly create and populate clear columns with True based on their predicted CC """
+
+        # several columns should be created so the average dollar total will very unlikely contain extreme cases (like all the high dollar ordres being cloudy)
+
+        for i in range(self.clear_columns):
+        # .loc function locates the "Clear" column for all orders
+        # .apply function applies True if collect is 'actually' clear otherwise False
+        # the 'Predicted_CC' is augmented by adding a random value between -.01 and .01 in order to make the chance of clear collect not equal to the predicted chance of clear collect
+        # random.random() is used to choose a 'actual' CLEAR percentage
+        # if the percent clear is greater than the augmented predicted percent, then the image is clear
+        # therefore the chance of a collect being clear is close to the predicted chance
+            self.active_orders[i] = self.active_orders.apply(lambda x: True
+                                                                if (random() > (x.Predicted_CC + (random() - .5) * .2) )
+                                                                else False, axis=1)     
+        
     def populate_dollar_values(self):
         """ Populate DollarPerSquare column with dollar values based on existing dollar value or customer """
 
@@ -127,18 +149,18 @@ class Revenue_Calculator:
 
         # change the dict values to the correct (starting) priorities
         for value in dollar_to_pri_map:
-            if value > 20: dollar_to_pri_map[value] = priority_list[0]
-            if value <= 20: dollar_to_pri_map[value] = priority_list[1]  
-            if value < 15: dollar_to_pri_map[value] = priority_list[2]
-            if value < 12: dollar_to_pri_map[value] = priority_list[3]
-            if value < 10: dollar_to_pri_map[value] = priority_list[4]
-            if value < 8:  dollar_to_pri_map[value] = priority_list[5]
-            if value < 6:  dollar_to_pri_map[value] = priority_list[6]
-            if value < 4:  dollar_to_pri_map[value] = priority_list[7]
-            if value < 3:  dollar_to_pri_map[value] = priority_list[8]
-            if value < 2:  dollar_to_pri_map[value] = priority_list[9]
-            if value < 1:  dollar_to_pri_map[value] = priority_list[10]      
-            if value == 0: dollar_to_pri_map[value] = priority_list[11]
+            if value >  self.dollar_breaks[0]: dollar_to_pri_map[value] = priority_list[0]
+            if value <= self.dollar_breaks[0]: dollar_to_pri_map[value] = priority_list[1]  
+            if value <  self.dollar_breaks[1]: dollar_to_pri_map[value] = priority_list[2]
+            if value <  self.dollar_breaks[2]: dollar_to_pri_map[value] = priority_list[3]
+            if value <  self.dollar_breaks[3]: dollar_to_pri_map[value] = priority_list[4]
+            if value <  self.dollar_breaks[4]:  dollar_to_pri_map[value] = priority_list[5]
+            if value <  self.dollar_breaks[5]:  dollar_to_pri_map[value] = priority_list[6]
+            if value <  self.dollar_breaks[6]:  dollar_to_pri_map[value] = priority_list[7]
+            if value <  self.dollar_breaks[7]:  dollar_to_pri_map[value] = priority_list[8]
+            if value <  self.dollar_breaks[8]:  dollar_to_pri_map[value] = priority_list[9]
+            if value <  self.dollar_breaks[9]:  dollar_to_pri_map[value] = priority_list[10]      
+            if value == self.dollar_breaks[10]: dollar_to_pri_map[value] = priority_list[11]
 
         # Set the priority value equal to the corresponding value in the dictionary using the pandas mapping function based on the dollar value
         self.active_orders['New_Priority'] = self.active_orders.DollarPerSquare.map(dollar_to_pri_map)
@@ -164,7 +186,7 @@ class Revenue_Calculator:
         # Set the total score
         self.active_orders.Total_Score = self.active_orders.apply( lambda x: (1 - x.Predicted_CC) * x.Score, axis=1)
     
-    def schedule_orders(self,case):
+    def schedule_orders(self):
         """ Return the list of orders that are have the maximum score within their respective 2 degree lat """
 
         latitude = self.min_latitude
@@ -176,29 +198,14 @@ class Revenue_Calculator:
                 max_index = order_list.Total_Score.idxmax()
                 self.active_orders.iloc[max_index, 8 ] = True
 
-            latitude += 2
-
-        # self.active_orders[case] = self.active_orders.Scheduled
-
-    def set_clear_orders(self):
-        """ Randomly populate change the scheduled order's clear column with True based on their predicted CC"""
-
-        # .loc function locates the "Clear" column for all orders that are activly scheduled 
-        # .apply function applies True if collect is 'actually' clear otherwise False
-        # the 'Predicted_CC' is augmented by adding a random value between -.01 and .01 in order to make the chance of clear collect not equal to the predicted chance of clear collect
-        # random.random() is used to choose a 'actual' CLEAR percentage
-        # if the percent clear is greater than the augmented predicted percent, then the image is clear
-        # therefore the chance of a collect being clear is close to the predicted chance
-        self.active_orders.Clear = self.active_orders.apply(lambda x: True
-                                                                if (random() > (x.Predicted_CC + (random() - .5) * .2) )
-                                                                else False, axis=1)                                                                                                  
+            latitude += 2                                                                                         
     
-    def total_dollars(self):
-        """ Returns the sum of all the dollars per square with a "Clear" value of True """
+    def total_dollars(self, column):
+        """ Returns the sum of all the dollars per square with a 'Clear' value of True for a given column"""
 
-        return self.active_orders.DollarPerSquare[(self.active_orders.Scheduled == True) & (self.active_orders.Clear == True)].sum()
+        return self.active_orders.DollarPerSquare[(self.active_orders.Scheduled == True) & (self.active_orders[column] == True)].sum()
 
-    def run_scenario(self, case):
+    def run_scenario(self):
         """ This will reassign each order with a random weather prediction and then reschedule orders accordingly and return a total dollar amount """
 
         # Reset the schedule by setting all 'Scheduled' and 'Clear' values to False
@@ -206,9 +213,14 @@ class Revenue_Calculator:
 
         # Choose a new random predicted weather value for each order, then recalculate other fields to produce the total dollars
         self.populate_total_score()
-        self.schedule_orders(case)
+        self.schedule_orders()
 
-        return self.total_dollars()
+        total = 0
+
+        for column in range(self.clear_columns):
+            total += self.total_dollars(column)
+
+        return total/self.clear_columns
 
     def run_priority_scheme(self, priority_scheme):
         """ Will run the set number of scenarios with a given prioritization scheme and return the average total dollar value """
@@ -220,7 +232,7 @@ class Revenue_Calculator:
         self.populate_priority(priority_scheme)
         self.populate_score()
 
-        total_dollars = self.run_scenario("1")
+        total_dollars = self.run_scenario()
 
         # Timing 
         end_time = time()
@@ -238,17 +250,17 @@ class Revenue_Calculator:
 
         if result.success:
             print(result)
-            plt.plot(result.x)
+            x_axis = [30] + self.dollar_breaks
+            plt.plot(x_axis, result.x)
             plt.show()
         else:
             raise ValueError(result.message)
         
-
     def run_test_cases(self):
         """ Will run the priority_scheme function for each test case priority set """
 
-        for count, scheme in enumerate(test_cases):
-            self.run_priority_scheme(scheme, count)
+        for scheme in test_cases:
+            self.run_priority_scheme(scheme)
         
 
 
@@ -277,3 +289,5 @@ if __name__ == "__main__":
 # 6.2 Return the total $ for this particular prioritization scheme
 # 6.3 Run model 100 times to simulate different cloud cover possibilities
 # 7 Use gradient decent to determine best prioritization using each pri value as a dimension  
+
+# 8 Create 10 columns where clear orders are set based on probability of being clear
