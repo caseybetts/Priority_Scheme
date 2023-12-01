96abbd72a13db40dff91286e2b02d1384c1cc298
Casey Betts <caseyandsusana@gmail.com>
2023-12-01 15:41:20 -0700

    Finds max value with cubic and plots all functions

--- pri_scheme.py
+++ pri_scheme.py
@@ -7,8 +7,8 @@ import json
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from math import exp
-from numpy import random
+from math import exp, cos
+from numpy import random, count_nonzero
 from os import listdir
 from scipy.optimize import minimize
 from sys import argv 
@@ -35,26 +35,29 @@ class Priority_Optimizer:
         self.cloud_cover_folder = parameters["clouds_folder"]
 
         # Optimization related variables
-        self.scale = .01
+        self.poly_degree = parameters["polynomial_degree"]
+        self.scale = 1
         self.optimization_method = parameters["optimization method"]
         self.optimization_tolerance = parameters["optimization tolerance"]
-        self.initial_coefficients = parameters["initial_coefficients"]
-        self.bounds = parameters["priority bounds"]
+        self.initial_coefficients = parameters["initial_coefficients"][-(self.poly_degree + 1):]
+        self.bounds = parameters["priority bounds"][-(self.poly_degree + 1):]
         self.average_prioritizations = []
         self.final_optimal_coefficients = []
-        self.number_of_dollar_val_buckets = len(parameters["dollar bin breakpoints"]) + 1
         self.pri_scheme_total_dollars = []
         self.coefficients = []
+        self.run_coefficients = []
+        self.iterated_coefficients = []
 
         # Dataframe related variables
         self.cloud_file_names = [x for x in listdir(self.cloud_cover_folder)]
         self.zero_dollar_customer_values = {int(k): v for k,v in parameters["zero_dollar_cust_dpsqkm"].items()}
         self.MCP_priority_to_dollar_map = {int(k): v for k,v in parameters["MCP_dollar_values"].items()}
-        self.dollar_breaks = parameters["dollar bin breakpoints"]
         self.weather_scenarios = parameters["number of weather scenarios"]
         self.test_priorities = parameters["test case priorities"]
         self.predicted_cc_uncertainty = parameters["predicted cloud cover uncertainty"] * 2                     # Standard Deviation of the normal distribution  
         self.cloud_cover_buckets = {}
+        self.weather_type = parameters["weather_type"]
+        self.scheduled_column_index = 0
 
         # Score Curve Variables
         self.coefficient = .47
@@ -64,6 +67,7 @@ class Priority_Optimizer:
         # Run initial functions
         self.load_data()
         self.prepare_dataframe()
+        self.startup_readout()
 
         
     def load_data(self):
@@ -86,7 +90,6 @@ class Priority_Optimizer:
         self.populate_actual_cc()
         self.populate_predicted_cc()
         self.populate_dollar_values()
-        self.populate_bucket()
 
         # Create dictionary template for mapping dollar values to priorities 
         self.dollar_to_pri_map = dict.fromkeys(set(self.active_orders.DollarPerSquare), 0) 
@@ -98,24 +101,34 @@ class Priority_Optimizer:
         self.active_orders.drop(labels=["Unnamed: 0"], axis=1, inplace=True)
 
         # Add needed columns
-        self.active_orders["Bucket"] = -1
         self.active_orders["New_Priority"] = 0
         self.active_orders["Score"] = 0
         self.active_orders["Total_Score"] = 0
         self.active_orders["Scheduled"] = False 
+
+        # Update the schedule column index accordingly
+        self.scheduled_column_index = self.active_orders.columns.get_loc("Scheduled")
             
     def find_cloud_cover(self, lat, lon):
         """ Given a latitude and longitude this will return the nearest cloud cover value from the PWOT .csv """
 
-        # round the lat and lon to the nearest .25
-        lat = round(lat * 4) / 4
-        lon = round(lon * 4) / 4
+        if self.weather_type == "clear":
+            return 0
+        elif self.weather_type == "cloudy":
+            return 90
+        elif self.weather_type == "random":
+            return rng.random()*90
+        elif self.weather_type == "PWOT":
 
-        try:
-            # return the single cloud cover value associated with the given lat and lon 
-            return self.cloud_cover[(self.cloud_cover.Latitude == lat) & (self.cloud_cover.Longitude == lon)].Value.iloc[0]
-        except:
-            print("Lat: ", lat, "Lon: ", lon)
+            # round the lat and lon to the nearest .25
+            lat = round(lat * 4) / 4
+            lon = round(lon * 4) / 4
+
+            try:
+                # return the single cloud cover value associated with the given lat and lon 
+                return self.cloud_cover[(self.cloud_cover.Latitude == lat) & (self.cloud_cover.Longitude == lon)].Value.iloc[0]
+            except:
+                print("Lat: ", lat, "Lon: ", lon)
     
     def populate_actual_cc(self):
         """ Choose an actual cloud cover score for each order """
@@ -135,11 +148,21 @@ class Priority_Optimizer:
         # Create list of column names
         for column_number in range(self.weather_scenarios):
 
-            # predicted cc is a random variation (+/- uncertanty) of the actual cc
-            self.active_orders["Predicted_" + str(column_number)] = self.active_orders.apply( lambda x: x["Actual_" + str(column_number)] + (rng.standard_normal() * self.predicted_cc_uncertainty), axis=1 )
+            if self.weather_type == "clear":
+                self.active_orders["Predicted_" + str(column_number)] = self.active_orders.apply( lambda x: 0, axis=1)
+            else:
+                # predicted cc is a random variation (+/- uncertanty) of the actual cc
+                self.active_orders["Predicted_" + str(column_number)] = self.active_orders.apply( lambda x: x["Actual_" + str(column_number)] + (rng.standard_normal() * self.predicted_cc_uncertainty), axis=1 )
 
-            # predicted cc must be between 0 and 1
-            self.active_orders["Predicted_" + str(column_number)].clip(lower=0, upper=1, inplace=True) 
+                # predicted cc must be between 0 and 1
+                self.active_orders["Predicted_" + str(column_number)].clip(lower=0, upper=1, inplace=True) 
+
+    def populate_MCP_dollar_value(self, x):
+        """ Returns a random dollar value normally distributed with a mean of the dollar value associated with the given MCP priority """
+
+        if x in self.MCP_priority_to_dollar_map.keys():
+
+            return self.MCP_priority_to_dollar_map[x] + rng.standard_normal()*.1 + .6
         
     def populate_dollar_values(self):
         """ Populate DollarPerSquare column with dollar values based on existing dollar value or customer """
@@ -150,44 +173,32 @@ class Priority_Optimizer:
 
         # There is a very large number of cust 306 orders so changing these based on priority will provide a much better model
         # For cust 306 change the dollar value based on priority using the MCP dollar mapping
-        self.active_orders.loc[self.active_orders.Cust_Num == 306, 'DollarPerSquare'] = self.active_orders.Task_Pri.map(self.MCP_priority_to_dollar_map) 
+        self.active_orders.loc[self.active_orders.Cust_Num == 306, 'DollarPerSquare'] = self.active_orders.Task_Pri.map(lambda x: self.populate_MCP_dollar_value(x)) 
 
-    def populate_bucket(self):
-        """ Each order is given a bucket number based on the dollar value of the order """
-
-        def bucketing_function(dollar_value):
-            """ Returns a bucket value given a dollar value """
-            
-            if dollar_value <= self.dollar_breaks[10]: return 0
-            if (dollar_value >  self.dollar_breaks[10]) and (dollar_value <= self.dollar_breaks[9]):  return 1
-            if (dollar_value >  self.dollar_breaks[9]) and (dollar_value <= self.dollar_breaks[8]):  return 2
-            if (dollar_value >  self.dollar_breaks[8]) and (dollar_value <= self.dollar_breaks[7]):  return 3
-            if (dollar_value >  self.dollar_breaks[7]) and (dollar_value <= self.dollar_breaks[6]):  return 4
-            if (dollar_value >  self.dollar_breaks[6]) and (dollar_value <= self.dollar_breaks[5]):  return 5
-            if (dollar_value >  self.dollar_breaks[5]) and (dollar_value <= self.dollar_breaks[4]):  return 6
-            if (dollar_value >  self.dollar_breaks[4]) and (dollar_value <= self.dollar_breaks[3]):  return 7
-            if (dollar_value >  self.dollar_breaks[3]) and (dollar_value <= self.dollar_breaks[2]):  return 8
-            if (dollar_value >  self.dollar_breaks[2]) and (dollar_value <= self.dollar_breaks[1]):  return 9
-            if (dollar_value >  self.dollar_breaks[1]) and (dollar_value <= self.dollar_breaks[0]):  return 10
-            if dollar_value > self.dollar_breaks[0]: return 11
-            
-        self.active_orders.Bucket = self.active_orders.apply( lambda x: bucketing_function(x.DollarPerSquare), axis=1)
-
-    def priority_function(self, coefficients, val):
+    def priority_function(self, coefficients, x):
         """ Defines the function for priority as a function of dollar value """
 
-        a,b,c,d,e = coefficients
+        # Return the value of a polynomial function based on the number of non-zero coefficients
+        # if self.poly_degree == 1:
+        #     d,e = coefficients
+        #     return d*(x-10) + e
+        # elif self.poly_degree == 2:
+        #     c,d,e = coefficients
+        #     return c*(x-10)**2 + d*(x-10) + e 
+        # elif self.poly_degree == 3:
+        #     b,c,d,e = coefficients
+        #     return b*(x-10)**3 + c*(x-10)**2 + d*(x-10) + e
+        # elif self.poly_degree == 4:
+        #     a,b,c,d,e = coefficients
+        #     return a*(x-10)**4 + b*(x-10)**3 + c*(x-10)**2 + d*(x-10) + e
+        
+        # Return the value of a trig function with 2 variables
+        a,b,c,d = coefficients
 
-        return a*(val-10)**4 + b*(val-10)**3 + c*(val-10)**2 + d*(val-10) + e
+        return a + b * (x-10) + c * (x-10)**2 + d * (x-10)**3
 
     def populate_priority(self, coefficients):
         """ Add a priority to each order based on the dollar value """
-
-        # Create a dictionary with key/value pairs of bucket/priority
-        # bucket_to_pri_map = dict()
-
-        # for bucket in range(self.number_of_dollar_val_buckets):
-        #     bucket_to_pri_map[bucket] = priority_list[-bucket]
         
         # Set the priority value based on the current dollar to priority funciton
         self.active_orders.New_Priority = self.active_orders.apply(lambda x: self.priority_function(coefficients, x.DollarPerSquare), axis=1)
@@ -222,7 +233,7 @@ class Priority_Optimizer:
             
             if not order_list.empty:
                 max_index = order_list.Total_Score.idxmax()
-                self.active_orders.iloc[max_index, 10 ] = True
+                self.active_orders.iloc[max_index, self.scheduled_column_index ] = True
 
             latitude += 2                                                                                         
     
@@ -234,7 +245,7 @@ class Priority_Optimizer:
         
         self.pri_scheme_total_dollars.append(total_dollars)
 
-        return total_dollars
+        return total_dollars + rng.standard_normal()
 
     def run_scenario(self, weather_column):
         """ This will reassign each order with a random weather prediction and then reschedule orders accordingly and return a total dollar amount """
@@ -250,14 +261,21 @@ class Priority_Optimizer:
 
     def run_priority_scheme(self, coefficients, weather_column):
         """ Will run the set number of scenarios with a given prioritization scheme and return the average total dollar value """
+                
+        # Unscale priorities
+        coefficients = [x / self.scale for x in coefficients ]
 
+    
         # Apply the given priority values to the orders
         self.populate_priority(coefficients)
         self.populate_score()
 
         total = self.run_scenario(weather_column)
+        self.run_coefficients.append(coefficients)
+
+        print("Coefficients: ", coefficients, " yeilded : $", -total)
         
-        return - total
+        return -total
     
     def optimal_priorities(self, weather_column):
         """ Uses the SciPy optimization tools to find the optimal prioritization scheme to maximize revenue for a given clear scenario """
@@ -268,8 +286,13 @@ class Priority_Optimizer:
         result = minimize(self.run_priority_scheme, 
                           scaled_initial_coefficients, 
                           args=weather_column, 
+                          bounds=scaled_bounds,
                           tol=self.optimization_tolerance, 
-                          method=self.optimization_method)
+                          method=self.optimization_method,
+                          options={"maxiter":150})
+        
+        self.iterated_coefficients.append(self.run_coefficients)
+        self.run_coefficients = []
 
         if result.success:
             return result
@@ -289,6 +312,7 @@ class Priority_Optimizer:
 
             optimization_result = self.optimal_priorities(weather_column)
             self.coefficients.append([x / self.scale for x in optimization_result.x])
+            print(optimization_result)
 
             # Timing and readout
             end_time = time()
@@ -322,6 +346,11 @@ class Priority_Optimizer:
 
         print("Running ", self.weather_scenarios," weather scenarios")
         print("Method: ", self.optimization_method)
+        print("self.scheduled_column_index :", self.scheduled_column_index)
+        print("Initial Coefficients: ", self.initial_coefficients)
+        
+        if count_nonzero(self.initial_coefficients) != self.poly_degree + 1:
+            print("WARNING: You will have unused coefficients since your polynomial degree is ", self.poly_degree)
 
     def display_results(self):
         """ Will print the resulting optimal priority function as well as display a graph of the all the results and the average result """
@@ -329,11 +358,25 @@ class Priority_Optimizer:
         print("\n\nThe final prioritization is: ", self.final_optimal_coefficients)
 
         x_axis = range(30)
+        i=0
+        result_colors = ['cornflowerblue', 'tan', 'lightgreen']
+        iter_colors = ['lavender', 'navajowhite', 'palegreen']
 
         for coefficients in self.coefficients:
-            plt.plot(x_axis, [self.priority_function(coefficients, x) for x in x_axis], color='lavender', linestyle='solid')
+            result_color = result_colors[i]
+            iter_color = iter_colors[i]
 
-        plt.plot(x_axis, [self.priority_function(self.final_optimal_coefficients, x) for x in x_axis], 'o-r')
+            # Plot a line for each iteration of the optimization run
+            for coefficients in self.iterated_coefficients[i]:
+                plt.plot(x_axis, [self.priority_function(coefficients, x) for x in x_axis], color=iter_color, linestyle='solid')
+
+            # Plot the final result from each optimization run
+            plt.plot(x_axis, [self.priority_function(coefficients, x) for x in x_axis], color=result_color, linestyle='solid')
+
+            i+=1
+    
+        # Plot the final total avreage from the results of all optimization runs
+        plt.plot(x_axis, [self.priority_function(self.final_optimal_coefficients, x) for x in x_axis], '--r')
         plt.show()
 
 
@@ -354,21 +397,17 @@ if __name__ == "__main__":
 """
 - Make the curve easy to change
 - Vet the current output and investigate
-- Make easy to change the number of pri bins
-
-+ Use PWOT weather file for weather data
-    + Create a trimmed PWOT .csv file
-    + Use PWOT to assign cc value to each order
-
-+ Contain the use of self.scale to only values entering and exiting the optimizer function
-- Create a Readout function prior to running the scenarios
-+ Add an Average readout to each optimizer run
-+ Add all prioritiztions to the resulting graph in gray and the average in red
 
 - Incoperate the max_cc into the scheduling logic
     - Use max_cc_weather_multiplyer =1/(Weather_Prediction - Max_CC + 1)
     - Cap the max_cc value at .8 (max_cc values close to 1 will cause the multiplyer to skyrocket)
 - Add weather file name to readout
 - Add a try block before creating the final .csv
+
+- Break code into multiple classes
+    - One class to initialize with the parameteres for the pri function (0 meaning that the term will not be used)
+    - One to run the optimization function 
++ add a bit of variance in dollar value between like orders so all $ values are different
+- Plot each iteration of the optimization function
 
 """
