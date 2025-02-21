9cd0077b23798e10aa85e0659d75218c0b0c725d
Casey Betts <caseyandsusana@gmail.com>
2025-02-21 12:50:37 -0700

    Updated to use argv and work with PROD

--- Active_Orders_DBF_to_CSV.py
+++ Active_Orders_DBF_to_CSV.py
@@ -1,23 +1,32 @@
 # Author: Casey Betts, 2023
 # This file holds the needed functions to receive a multisensor_active_orders_ufp .dbf file and output a cleaned .csv
+# To use type the command: python .\Active_Orders_DBF_to_CSV.py [address to .dbf file]
+# You will be prompted for the source type (DRIFS/PROD)
 
 import pandas as pd
 import geopandas as gpd 
+from os import path
+from sys import argv, exit 
 
-def get_user_info():
+# Get the location and name
+loc_dbf = argv[1]
+file_location, filename = path.split(loc_dbf)
+filename = filename[:-4]
+
+def get_source():
     """ Collects needed info from the user via the console """
-    # Gather the file location
-    file_location = input("Enter Folder Location: ")
-    filename = input("Enter File Name: ")
 
     # Gather the data source so we know which column names to drop
     while True:
         source = input("Enter data source ('DRIFS' or 'PROD') or 'Q' to quit: ")
-        if not source in ['DRIFS', 'PROD', 'Q', 'q']:
+        if source in ['DRIFS', 'PROD']:
+            break
+        elif source in ['Q', 'q']:
+            exit()
+        else:
             print("Please input 'DRIF', 'PROD' or 'Q'")
-        else: break
 
-    return [file_location, filename, source]
+    return source
     
 def load_dbf_to_dataframe(file_location):
     """ Using the dbf file location of the multisensor_active_orders_ufp, 
@@ -57,18 +66,30 @@ def clean_df(df, source):
                             'Test_Pri', 'geometry']
         columns_to_rename = {"SAP_CUS": "Cust_Num", "TASK_PRIOR": 'Task_Pri',"DOLPERSQKM": "DollarPerSquare"}
     elif source == 'PROD':
-        columns_to_drop = [ 'classifica','data_acces','demand_typ','standing_t','order_numb',
-                            'line_numbe','order_iden','external_i','sap_custom','order_desc',
+        columns_to_drop = [ 'classifica','data_acces','standing_t','order_numb',
+                            'line_numbe','order_iden','external_i','order_desc',
                             'area_remai','product_le','order_stat','active_tim','start_date',
-                            'end_date','area','pricing','sap_con','tasking_pr','ssr_priori',
+                            'end_date','area','pricing','sap_con', 'ssr_priori',
                             'backhaul_p','responsive','min_ona','max_ona','min_sun_az',
                             'max_sun_az','min_sun_el','max_sun_el','min_tar_az','max_tar_az',
-                            'max_cloud_','purchase_o','purchase_1','country','stereoprod',
-                            'vehicle','ge01','wv01','wv03','lg01','lg02','imagebands',
+                            'purchase_o','purchase_1','country','stereoprod',
+                            'ge01','wv01','wv03','lg01','lg02','imagebands',
                             'percent_co','type','max_asymme','min_asymme','max_bisect',
                             'min_bisect','max_conver','min_conver','consider_f','consider_1',
-                            'lg01_sched','lg02_sched','scan_direc','line_rate','tdi_flag','tdi_offset',]
-        columns_to_rename = { 'sap_custom': 'Cust_Num', 'tasking_pr': 'Task_Pri', 'price_per_': 'DollarPerSquare' }
+                            'lg01_sched','lg02_sched','scan_direc','line_rate','tdi_flag',
+                            'tdi_offset','accesses_2','access_veh','accesses_7','accesses_3',
+                            'parent_ord', 'project_no', 'customer_t', 'price_rema', 'time_of_da',
+                            'downlink_l', 'downlink_1', 'paired_ord',
+                            'production', 'max_snow_c', 'is_partial', 'min_partia', 'selected_v',
+                            'requested_', 'lg03', 'lg04', 'lg05', 'lg06', 'requested1',
+                            'max_collec', 'lg03_sched', 'lg04_sched', 'lg05_sched', 'lg06_sched',
+                            'tdi_pan', 'tdi_ms1_2', 'tdi_ms3_4', 'tdi_ms5_6', 'tdi_ms7_8',
+                            'tdi_ms1_3', 'tdi_ms2_4', 'tdi_ms1_ni', 'tdi_ms2_re', 'tdi_ms3_gr',
+                            'tdi_ms4_bl', 'tdi_ms5_re', 'tdi_ms6_re', 'tdi_ms7_ye', 'tdi_ms8_co',
+                            'exposure_s', 'exposure_1', 'exposure_c', 'Shape_Leng', 'Shape_Area',
+                            'geometry','wv02', 'calibratio', 'mps_projec', 'factory', 'increment_'
+                            ]
+        columns_to_rename = { 'sap_custom': 'Cust_Num', 'tasking_pr': 'Task_Pri', 'price_per_': 'DollarPerSquare', 'max_cloud_': 'MAX_CC' }
    
     df.drop(labels=columns_to_drop, axis=1, inplace=True)
     df.rename(columns=columns_to_rename, inplace=True)
@@ -85,16 +106,16 @@ def dataframe_to_csv(dataframe, filename):
 if __name__ == "__main__":
 
     # Get the user info
-    user_info = get_user_info()
+    source = get_source()
 
     # Load in the .dbf to a pandas dataframe
-    df = load_dbf_to_dataframe(user_info[0] + '\\' + user_info[1])
+    df = load_dbf_to_dataframe(file_location + '\\' + filename)
 
     # Clean the dataframe 
-    cleaned_df = clean_df(df, user_info[2])
+    cleaned_df = clean_df(df, source)
     
     # Save dataframe to a .csv file
-    dataframe_to_csv(cleaned_df, user_info[1])
+    dataframe_to_csv(cleaned_df, filename)
 
 
 
